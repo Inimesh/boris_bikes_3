082f8d091b965fd101c5676f09323b6aacbdd84d
Evie <evie@Evies-MacBook-Air.local>
2021-11-16 16:34:16 +0000

    Added a release method and its existence test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-     
+    it 'testing release bike' do
+        docking_station = DockingStation.new
+        expect(docking_station).to respond_to :release_bike
+    end
 end
-
