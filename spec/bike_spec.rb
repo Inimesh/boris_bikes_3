d5a4a3c2b32172e5c3d9ce8d774c378b719e8a71
Inimesh <ijmeshoulam@gmail.com>
2021-11-22 13:45:09 +0000

    bike_spec refactor

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,13 +1,9 @@
 require 'bike'
 
 describe Bike do
-    it 'responds to the method working?' do
-        bike = Bike.new
-        expect(bike).to respond_to :working
-    end
-
     it 'tells you if the bike is working' do
 		bike = Bike.new
 		expect(bike.working).to eq true
 	end
+
 end
